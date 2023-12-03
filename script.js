c17292436890c35dc5496b08cf032044e1354136
WILDERGH <152807347+WILDERGH@users.noreply.github.com>
2023-12-03 16:08:34 -0600

    Add files via upload
    Examen de Diseño y programación Web

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // Desplazamiento suave al hacer clic en los enlaces de la barra de navegación
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
